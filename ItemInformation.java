e6aca0683daa16ca2885eed4662b080fd2716633
Kaii 카이 <89370178+SenorKaii@users.noreply.github.com>
2022-07-06 22:20:55 +0800

    Update ItemInformation.java

--- ItemInformation.java
+++ ItemInformation.java
@@ -3,6 +3,7 @@ public class ItemInformation {
     private String itemId, itemName, datePurchase;
     private double itemPrice;
     
+    //Constructor
     public ItemInformation(CustomerInformation ci, String itemId, String itemName, String datePurchase, double itemPrice){
         this.ci = ci;
         this.itemId = itemId;
@@ -14,15 +15,18 @@ public class ItemInformation {
     //public String getCustId() {return ci.custId;}
     //public String getCustIc() {return ci.custIc;}
     //public boolean isCounterPaid() {return ci.counterPaid;}
-
+    
+    //Acessor
     public String getItemID() {return itemId;}
     public String getItemName() {return itemName;}
     public String getDatePurchase() {return datePurchase;}
     public double getItemPrice(){return itemPrice;}
     public CustomerInformation getCustomer() {return ci;}
     
+    //Mutator
     public void setCounterPaid(int n) {ci.setCounterPaid(n);}
     
+    //toString
      public String toString(){
         return "Item Information "
             + "\nItem ID: " + itemId 
